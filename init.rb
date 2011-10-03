b05e75bc650a2998ef73ab92d4b5e3a7e488bc9c
Akiko Takano <akiko.pusu@gmail.com>
2011-10-03 16:08:39 +0900

    Admin menuにアイコンが出るように調整してみました

--- init.rb
+++ init.rb
@@ -15,6 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 require 'redmine'
+require 'admin_menu_hooks'
 
 Redmine::Plugin.register :redmine_logs do
   name 'Redmine Logs plugin'
