1c2175ecffb697325e822b6dba46010b5061a84e
haru_iida <devnull@localhost>
2012-01-07 22:42:25 +0900

    version was changed.

--- init.rb
+++ init.rb
@@ -22,7 +22,7 @@ Redmine::Plugin.register :redmine_logs do
   author 'Haruyuki Iida'
   url "http://www.r-labs.org/projects/logs" if respond_to?(:url)
   description 'This is a Logs plugin for Redmine'
-  version '0.0.2'
+  version '0.0.3'
   requires_redmine :version_or_higher => '1.2.0'
   
   menu :admin_menu, :redmine_logs, { :controller => 'logs', :action => 'index'}, :caption => :logs
