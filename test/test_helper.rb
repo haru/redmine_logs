0f540e776b9c566ac78f89e7f1ce4d55e6fb67af
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2012-05-19 14:26:58 +0900

    using simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
 # Load the normal Rails helper
 require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start 'rails'
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
