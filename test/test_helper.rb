063d578dd01e7e0c1ef130c8845ea9b43e8929c6
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2012-10-27 16:32:25 +0900

    fixed Simplecov parameter.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@
 require 'simplecov'
 require 'simplecov-rcov'
 SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
-SimpleCov.start 'rails'
+SimpleCov.start
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
 # Ensure that we are using the temporary fixture path
