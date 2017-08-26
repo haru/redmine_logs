f900ba6aa1cafbbe4dcb82b02f29a1b3ba55cf31
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2017-08-26 22:18:13 +0900

    add test

--- test/functional/logs_controller_test.rb
+++ test/functional/logs_controller_test.rb
@@ -2,8 +2,36 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 #require File.dirname(__FILE__) + '/../test_helper'
 
 class LogsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :projects, :users, :roles, :members
+  LOGDIR = "#{Rails.root.to_s}/log"
+  def setup
+    @request    = ActionController::TestRequest.new
+    @request.session[:user_id] = 1
+
+    File.open("#{LOGDIR}/foo.log", "w") do |f|
+      f.puts("Hello, World!")
+      f.puts("Hello, World!")
+      f.puts("Hello, World!")
+    end
+  end
+
+  def test_index
+    get :index
+    assert_response :success
+  end
+
+  def test_download
+    get :download, path: "#{LOGDIR}/foo.log"
+    assert_response :success
+  end
+
+  def test_show
+    post :show, path: "#{LOGDIR}/foo.log"
+    assert_response :success
+  end
+
+  def test_delete
+    post :delete, path: "#{LOGDIR}/foo.log"
+    assert_response :redirect
   end
 end
