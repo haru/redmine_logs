1f27bef0eb76a2c9fc878b927ba631bb3e315977
maro-kt <k.tsutsumi.0224@gmail.com>
2018-09-05 14:59:30 +0900

    Remove function "Delete logs"

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,4 @@ RedmineApp::Application.routes.draw do
   match 'logs/index', :controller => 'logs', :action => 'index', :via => [:get, :post]
   match 'logs/show', :controller => 'logs', :action => 'show', :via => [:get, :post]
   match 'logs/download', :controller => 'logs', :action => 'download', :via => [:get, :post]
-  match 'logs/delete', :controller => 'logs', :action => 'delete', :via => [:get, :post]
 end
