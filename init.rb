c0f834b21ca326132b3fd7ccb4fc28a4816c0eaf
kumojima <kumojima@gmail.com>
2022-04-14 15:43:40 +0900

    Redmine5対応 (#6)

--- init.rb
+++ init.rb
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 require 'redmine'
-require 'admin_menu_hooks'
+require File.expand_path('lib/admin_menu_hooks', __dir__)
 
 Redmine::Plugin.register :redmine_logs do
   name 'Redmine Logs plugin'
